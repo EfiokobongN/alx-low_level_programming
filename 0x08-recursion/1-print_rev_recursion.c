d8305905727eff63db62b4a23853cf5dcd60e863
EfiokobongN <mailpreciousnelson@gmail.com>
2023-08-02 06:49:34 -0700

    ALX 0x08 C project

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * 1-print_rev_recursion.c - function to print reverse string
+ * _print_rev_recursion.c - function to print reverse string
  * @s: string being passed
  * Return: Always 0.
  */

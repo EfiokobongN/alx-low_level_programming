94ed0d84f57d707754a391823e054ab9b01bbb0f
EfiokobongN <mailpreciousnelson@gmail.com>
2023-08-29 03:38:36 -0700

    singly linked

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,26 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all elements of a listint_t
+ * print_listint - prints all elements of a listint_t
  *
  * @h: h linklist node
  *
  * Return: numbers of nodes
  *
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-		printf("[%u] %s\n", h->len, h->str);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
-
 	return (count);
 }

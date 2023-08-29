5b4f8f74f7dd5789d2f4a0240915be8c933c13f2
EfiokobongN <mailpreciousnelson@gmail.com>
2023-08-29 04:55:01 -0700

    Linke singly project

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,10 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef LISTSH
+#define LISTSH
+#include <stddef.h>
 
-#include<string.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -31,4 +28,10 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+int _putchar(char c);
+
 #endif
